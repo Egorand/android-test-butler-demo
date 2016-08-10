35dc01b8322e69c71fceaffa3e3bc1417aa5928f
Egor Andreevici <andreevich.egor@gmail.com>
2016-08-10 20:49:55 +0200

    Initial commit

---
+++ app/src/androidTest/kotlin/me/egorand/testbutlerdemo/OrientationChangeTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 Egor Andreevici
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package me.egorand.testbutlerdemo
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class OrientationChangeTest {
+
+    @Rule @JvmField val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test fun shouldDisplayMasterFragmentInPortrait() {
+        rotateToPortrait()
+
+        onView(withText(R.string.master_message)).check(matches(isDisplayed()))
+    }
+
+    @Test fun shouldDisplayMasterAndDetailFragmentsInLandscape() {
+        rotateToLandscape()
+
+        onView(withText(R.string.master_message)).check(matches(isDisplayed()))
+        onView(withText(R.string.detail_message)).check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/main/kotlin/me/egorand/testbutlerdemo/DetailFragment.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Egor Andreevici
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package me.egorand.testbutlerdemo
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.layout_fragment.*
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+            inflater.inflate(R.layout.layout_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        console.text = getString(R.string.detail_message)
+    }
+}

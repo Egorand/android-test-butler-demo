0c95d5d3817f07f2a4a028750b7d4315bd26c3c2
Egor Andreevici <andreevich.egor@gmail.com>
2016-08-12 08:28:44 +0200

    Removed redundant @RunWith annotations

--- app/src/androidTest/kotlin/me/egorand/testbutlerdemo/OrientationChangeTest.kt
+++ app/src/androidTest/kotlin/me/egorand/testbutlerdemo/OrientationChangeTest.kt
@@ -21,12 +21,9 @@ import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class OrientationChangeTest {
 
     @Rule @JvmField val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
--- app/src/androidTest/kotlin/me/egorand/testbutlerdemo/SavingInstanceStateTest.kt
+++ app/src/androidTest/kotlin/me/egorand/testbutlerdemo/SavingInstanceStateTest.kt
@@ -22,12 +22,9 @@ import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class SavingInstanceStateTest {
 
     @Rule @JvmField val activityTestRule = ActivityTestRule<InputActivity>(InputActivity::class.java)
